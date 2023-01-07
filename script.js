18f12bb1348344606fc7d955c144c9ae9d542617
AzimAhmedBijapur <azeembijapur786@gmail.com>
2023-01-08 00:32:23 +0530

    password validation and form restructuring

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var regexName=new RegExp('[a-zA-Z]');
-
 var submit = document.querySelector('form');
 
 var buttonSubmit= document.querySelector('#create-acc');
@@ -7,21 +5,39 @@ var buttonSubmit= document.querySelector('#create-acc');
 buttonSubmit.addEventListener('click',()=>{
 
     var valid=submit.checkValidity();
+    var password=validatePassword();
     
-    if( valid==true)
+    if( valid==true && password==true)
         {
             submit.submit();
         }
-    
+    else{
+        document.getElementById('pw').value='';
+        document.getElementById('cpw').value='';
+    }
     
 });
 
-function validName(){
+function validatePassword(){
     
-    var fname_label=document.querySelector('#fname-label');
+    var pw=document.getElementById('pw').value;
+    var cpw=document.getElementById('cpw').value;
+    var reg=new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$');
 
-    fname_label.innerHTML='Yes';
-
-    console.log('yes');
+    if(pw==cpw && reg.test(pw)){
+        return true
+    }
+    else
+    {
+        if(reg.test(pw)==false && pw!='' && cpw!='')
+        {
+            document.querySelector('.error-msg').textContent='*Password must contain uppercase, numeric and special character';
+        }
+        else{
+            document.querySelector('.error-msg').textContent='*Passwords does not match';
+        }
+        return false;
+    }
+    
     
 }
